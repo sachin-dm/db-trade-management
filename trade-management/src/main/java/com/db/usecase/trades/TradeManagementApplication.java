6503160833a3ba9539ccb0ff361ad37896763854
smhetre <smhetre@tracelink.com>
2021-03-26 14:43:54 +0000

    Initial Commit

---
+++ trade-management/src/main/java/com/db/usecase/trades/TradeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.db.usecase.trades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeManagementApplication.class, args);
+	}
+
+}
